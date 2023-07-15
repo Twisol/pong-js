f1ce6b68172cae910f07a7efcdb4a7f09c9ac9a3
Jonathan Castello <twisolar@gmail.com>
2023-07-15 16:18:42 -0700

    Draw the paddles and ball

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,41 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
+// Position as a percentage: 0 is all the way up, 1 is all the way down.
+let leftPaddlePosition  = 0.5;
+let rightPaddlePosition = 0.5;
+
+const paddleWidth  =  10.0;
+const paddleHeight = 100.0;
+
+const ballWidth  = 5.0;
+const ballHeight = 5.0;
+
+// Background
 ctx.fillStyle = "grey";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// Left paddle
+ctx.fillStyle = "red";
+ctx.fillRect(
+  10,
+  leftPaddlePosition * (canvas.height - paddleHeight),
+  paddleWidth,
+  paddleHeight,
+);
+
+// Right paddle
+ctx.fillStyle = "red";
+ctx.fillRect(
+  (canvas.width - 10) - paddleWidth,
+  rightPaddlePosition * (canvas.height - paddleHeight),
+  paddleWidth,
+  paddleHeight);
+
+// Ball
+ctx.fillStyle = "red";
+ctx.fillRect(
+  (canvas.width / 2.0) - (ballWidth / 2.0),
+  (canvas.height / 2.0) - (ballHeight / 2.0),
+  ballWidth,
+  ballHeight);
